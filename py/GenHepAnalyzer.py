a151208ed5f6a14644700eda345cb0f34f191bed
VitalyVorobyev <vit.vorobiev@gmail.com>
2019-01-21 18:34:16 +0700

    Modes splitter: first version

--- py/GenHepAnalyzer.py
+++ py/GenHepAnalyzer.py
@@ -1,7 +1,135 @@
 """ """
 
+from glob import glob
+import numpy as np
+
+from ROOT import TChain, gSystem
+
+tuplePath = '/home/vitaly/work/DsjInc/tuples'
+
+def sigMCFile(ty, st):
+    """ Get root file name """
+    return glob('/'.join([tuplePath, 'signt', '_'.join(['dsjinc', 'sigmc', 'ty' + ty, 'st' + str(st), '*.root'])]))
+
+ids = {
+        'Dsj0+'  :  10431,
+        'Dsj0-'  : -10431,
+        'Dsj1+'  :  20433,
+        'Dsj1-'  : -20433,
+        'Ds+'    :  431,
+        'Ds-'    : -431,
+        'Ds*+'   :  433,
+        'Ds*-'   : -433,
+        'gamma'  :  22,
+        'pi0'    :  111,
+        'phi'    :  333,
+        'pi+'    :  211,
+        'pi-'    : -211,
+        'K+'     :  321,
+        'K-'     : -321,
+        'K*0'    :  313,
+        'K*0bar' : -313,
+        'Ks0'    :  310
+    }
+
+modesDict = {
+    'Dsj+' : {
+        0 : set([ids['Ds+'], ids['gamma']]),
+        1 : set([ids['Ds+'], ids['pi0']]),
+        2 : set([ids['Ds+'], ids['pi+'], ids['pi-']]),
+        3 : set([ids['Ds+'], ids['pi0'], ids['pi0']])
+    },
+    'Dsj-' : {
+        0 : set([ids['Ds-'], ids['gamma']]),
+        1 : set([ids['Ds-'], ids['pi0']]),
+        2 : set([ids['Ds-'], ids['pi+'], ids['pi-']]),
+        3 : set([ids['Ds-'], ids['pi0'], ids['pi0']])
+    },
+    'Ds+' : {
+        0 : set([ids['phi'], ids['pi+']]),
+        1 : set([ids['K*0'], ids['K+']]),
+        2 : set([ids['Ks0'], ids['K+']])
+    },
+    'Ds-' : {
+        0 : set([ids['phi'],    ids['pi-']]),
+        1 : set([ids['K*0bar'], ids['K-']]),
+        2 : set([ids['Ks0'],    ids['K-']])
+    }
+}
+
 class GenHepAnalyser(object):
     """ """
-    def __init__(self):
+    def __init__(self, dstree):
         """ """
+        self.chain = dstree
+        self.chain.GetEvent(0)
+        # print(dir(self.chain))
+        # return
+        self.idhep = self.chain.mcevt.genhep.idhep
+        self.daF = self.chain.mcevt.genhep.daF
+        self.daL = self.chain.mcevt.genhep.daL
+        self.exp = self.chain.evt.info.exp
+        self.evtn = self.chain.evt.info.evtn
+        self.data = np.empty(self.chain.GetEntries(), dtype=[
+            ('exp', np.int32),
+            ('evtn', np.int32),
+            ('ds+', np.int32),
+            ('ds-', np.int32),
+            ('dsj+', np.int32),
+            ('dsj-', np.int32),
+            ])
+        self.run()
         
+        # self.vars['tmode'] = self.findDecay(ids['Dsj0+'], 'Dsj') - self.findDecay(ids['Dsj0-'], 'Dsj')\
+            #    + 100*self.findDecay(ids['Dsj1+'], 'Dsj') - 100*self.findDecay(ids['Dsj1-'], 'Dsj')
+        # self.vars['tdsmode'] = self.findDecay(ids['Ds+'], 'Ds') - self.findDecay(ids['Ds-'], 'Ds')
+
+    def run(self):
+        """ Event loop """
+        for idx, _ in enumerate(self.chain):
+            if idx % 10000 == 0:
+                print('{} events'.format(idx))
+            dsjp = self.findDecay(ids['Dsj0+'], 'Dsj+')
+            if dsjp == -1:
+                dsjp = self.findDecay(ids['Dsj1+'], 'Dsj+')
+                if dsjp >= 0:
+                    dsjp = dsjp * 10**4
+            dsjn = self.findDecay(ids['Dsj0-'], 'Dsj-')
+            if dsjn == -1:
+                dsjn = self.findDecay(ids['Dsj1-'], 'Dsj-')
+                if dsjn >= 0:
+                    dsjn = dsjn * 10**4
+            self.data[idx] = (self.exp, self.evtn,
+                self.findDecay(ids['Ds+'], 'Ds+'),
+                self.findDecay(ids['Ds-'], 'Ds-'),
+                dsjp, dsjn)
+
+    def findDecay(self, idhep, key):
+        """ Identify mode in the EvtGen table """
+        nphep = np.array(self.idhep)
+        idxList = np.nonzero(nphep == idhep)
+        if len(idxList[0]) == 0:
+            return -1
+        idx = idxList[0][0]
+        fsp = set([self.idhep[i] for i in np.arange(self.daF[idx], self.daL[idx]+1)])
+        for mode, fsps in modesDict[key].iteritems():
+            print('{} vs. {}'.format(fsp, fsps))
+            if fsp == fsps:
+                return mode
+        return -1
+
+def main():
+    """ Unit test """
+    print(modesDict)
+    gSystem.Load('libRecoObj.so')
+    gSystem.Load('libdsjdata.so')
+    chain = TChain('ds', 'ds')
+    for fname in sigMCFile('dsst0', 1):
+        chain.Add(fname)
+    print('{} Ds candidates'.format(chain.GetEntries()))
+    gha = GenHepAnalyser(chain)
+    print(gha.data[:10])
+    np.save('mctruth.npy', gha.data)
+
+if __name__ == '__main__':
+    main()
--- py/ModeSplitter.py
+++ py/ModeSplitter.py
@@ -14,204 +14,247 @@ from selections import DsStr
 
 tuplePath = '/home/vitaly/work/DsjInc/tuples'
 
-ids = {
-        'Dsj0+'  :  10431,
-        'Dsj0-'  : -10431,
-        'Dsj1+'  :  20433,
-        'Dsj1-'  : -20433,
-        'Ds+'    :  431,
-        'Ds-'    : -431,
-        'Ds*+'   :  433,
-        'Ds*-'   : -433,
-        'gamma'  :  22,
-        'pi0'    :  111,
-        'phi'    :  333,
-        'pi+'    :  211,
-        'pi-'    : -211,
-        'K+'     :  321,
-        'K-'     : -321,
-        'K*0'    :  313,
-        'K*0bar' : -313,
-        'Ks0'    :  310
-    }
-
-modesDict = {
-    'Dsj' : {
-        0 : [set([ids['Ds+'], ids['gamma']]), set([ids['Ds-'], ids['gamma']])],
-        1 : [set([ids['Ds+'], ids['pi0']]),   set([ids['Ds-'], ids['pi0']])],
-        2 : [set([ids['Ds+'], ids['pi0']]),   set([ids['Ds-'], ids['pi0']])]
-    },
-    'Ds' : {
-        0 : [set([ids['phi'], ids['pi+']]), set([ids['phi'],    ids['pi-']])],
-        1 : [set([ids['K*0'], ids['K-']]),  set([ids['K*0bar'], ids['K+']])],
-        2 : [set([ids['Ks0'], ids['K+']]),  set([ids['Ks0'],    ids['K-']])]
-    }
-}
-
 def sigMCFile(ty, st):
     """ Get root file name """
-    print('/'.join([tuplePath, 'signt', '_'.join(['dsjinc', 'sigmc', 'ty' + ty, 'st' + str(st), '*.root'])]))
     return glob('/'.join([tuplePath, 'signt', '_'.join(['dsjinc', 'sigmc', 'ty' + ty, 'st' + str(st), '*.root'])]))
-
-def Branches(mode):
-    """ Set of branches """
-    common = {  # all modes
-        'i'  : ['exp', 'run', 'evtn', 'mode', 'dsmode', 'flv', 'h_ds_id', 'h_ds_flag', 'tmode', 'tdsmode'],
-        'f'  : ['m', 'mds', 'pcmds', 'cos_hel_dsj'],
-        'iv' : ['h_ds_ch']
-    }
-
-    vec = {  # phi or K*0 fomr Ds
-        # 'i'  : ['h1_vec_id', 'h1_vec_flag', 'h2_vec_id', 'h2_vec_flag'],
-        'f'  : ['mvec', 'cos_hel_vec'],
-        # 'iv' : ['h1_vec_ch', 'h2_vec_ch', 'h_ds_ch']
-    }
-
-    gamDsj = {  # gamma from Dsj
-        'i'  : ['gam_dsj_id', 'gam_dsj_flag'],
-        'f'  : ['gam_dsj_e',  'gam_dsj_costh'],
-        'iv' : ['gam_dsj_ch']
-    }
-
-    pi0Dsj = {  # pi0 from Dsj
-        'i'  : ['gam_dsj_id', 'gam_dsj_flag'],
-        'f'  : ['pi0_dsj_eg1', 'pi0_dsj_eg2', 'pi0_dsj_p',  'gam_dsj_costh'],
-        'iv' : ['pi0_dsj_ch']
-    }
-
-    modeDict = {
-        0 : [common, vec, gamDsj],  # Ds gamma
-        1 : [common, vec, pi0Dsj],  # Ds pi0
-    }
-    return {key : [val for sub in modeDict[mode] for val in sub] for key in common.keys()}
 
 class ModeSplitter(object):
     """ Mode Splitted for DsjInc """
-    mds = 1968.34 # +- 0.07 MeV
+    mds = 1.96834 # +- 0.07 MeV
 
     def __init__(self, name, ty, st, modes):
         """ Constructor """
         self.name = name
         self.ty = ty
-        self.st = st
+        self.st = str(st)
         self.modes = modes
+        self.t = {}
+        self.vars = {}
         self.getChain()
+        self.initVarDicts()
+
+    def initVarDicts(self):
+        """ Define variables """
+        self.branches = {
+        'common' : {  # all modes
+            'i' : {
+                'exp'   : lambda: self.info.exp,
+                'run'   : lambda: self.info.run,
+                'evtn'  : lambda: self.info.evtn,
+                'mode'  : lambda: self.evt.mode,
+                'dsmode': lambda: self.evt.mode_ds,
+                'flv'   : lambda: self.evt.flv
+            },
+            'f' : {
+                'm'          : lambda: self.evt.m - self.evt.mds + ModeSplitter.mds,
+                'mds'        : lambda: self.evt.mds,
+                'pcmds'      : lambda: self.evt.pcmsds,
+                'cos_hel_dsj': lambda: self.evt.cos_hel_dsj,
+                'mvec'       : lambda: self.evt.mvec,
+                'cos_hel_vec': lambda: self.evt.cos_hel_vec
+            }
+        },
+        'commonMC' : {
+            'i'  : {
+                'h_ds_id'  : lambda: self.mcevt.h_ds_gen.id,
+                'h_ds_flag': lambda: self.mcevt.h_ds_gen.flag,
+                'ds_flag'  : lambda: self.mcevt.ds_gen.flag,
+                'dsj_flag' : lambda: self.mcevt.dsj_gen.flag,
+                'tmode'    : lambda: self.mcevt.gam_dsj_gen.id,
+                'tdsmode'  : lambda: self.mcevt.gam_dsj_gen.flag
+            },
+            'iv' : {
+                'h_ds_ch'  : lambda: self.mcevt.h_ds_gen.chain,
+            }
+        },
+        'gamDsj' : {
+            'f' : {
+                'gam_e'     : lambda: self.evt.gam_dsj.e,
+                'gam_costh' : lambda: self.evt.gam_dsj.costh
+            }
+        },
+        'gamDsjMC' : {
+            # 'i' : {
+            #     'gam_id'   : lambda: self.mcevt.gam_dsj_gen.id,
+            #     'gam_flag' : lambda: self.mcevt.gam_dsj_gen.flag
+            # },
+            'iv' : {
+                'gam_ch' : lambda: self.mcevt.gam_dsj_gen.chain
+            }
+        },
+        'pi0Dsj' : {
+            'f' : {
+                'pi0_eg1'   : lambda: min(self.evt.pi0_dsj.eg1, self.evt.pi0_dsj.eg2),
+                'pi0_eg2'   : lambda: max(self.evt.pi0_dsj.eg1, self.evt.pi0_dsj.eg2)
+            }
+        },
+        'pi0DsjMC' : {
+            'i' : {
+                'pi0_id'   : lambda: self.mcevt.pi0_dsj_gen.id,
+                'pi0_flag' : lambda: self.mcevt.pi0_dsj_gen.flag
+            },
+            'iv' : {
+                'pi0_ch' : lambda: self.mcevt.pi0_dsj_gen.chain
+            }
+        },
+        'pi+pi-' : {
+            'f' : {
+                'pip_atckpi' : lambda: self.evt.pip_dsj.atckpi,
+                'pip_px'     : lambda: self.evt.pip_dsj.p[0],
+                'pip_py'     : lambda: self.evt.pip_dsj.p[1],
+                'pip_pz'     : lambda: self.evt.pip_dsj.p[2],
+                'pim_atckpi' : lambda: self.evt.pim_dsj.atckpi,
+                'pim_px'     : lambda: self.evt.pim_dsj.p[0],
+                'pim_py'     : lambda: self.evt.pim_dsj.p[1],
+                'pim_pz'     : lambda: self.evt.pim_dsj.p[2]
+            }
+        },
+        'pi+pi-MC' : {
+            'i' : {
+                'pip_id'   : lambda: self.mcevt.pip_dsj_gen.id,
+                'pip_flag' : lambda: self.mcevt.pip_dsj_gen.flag,
+                'pim_id'   : lambda: self.mcevt.pim_dsj_gen.id,
+                'pim_flag' : lambda: self.mcevt.pim_dsj_gen.flag
+            },
+            'iv' : {
+                'pip_ch' : lambda: self.mcevt.pip_dsj_gen.chain,
+                'pim_ch' : lambda: self.mcevt.pim_dsj_gen.chain
+            }
+        },
+        'pi0pi0' : {
+            'f' : {
+                'pi0lo_eg1' : lambda: min(self.evt.pi0lo_dsj.eg1, self.evt.pi0lo_dsj.eg2),
+                'pi0lo_eg2' : lambda: max(self.evt.pi0lo_dsj.eg1, self.evt.pi0lo_dsj.eg2)
+            }
+        },
+        'pi0pi0MC' : {
+            'i' : {
+                'pi0lo_id'   : lambda: self.mcevt.pi0lo_dsj_gen.id,
+                'pi0lo_flag' : lambda: self.mcevt.pi0lo_dsj_gen.flag
+            },
+            'iv' : {
+                'pi0lo_ch' : lambda: self.mcevt.pi0lo_dsj_gen.chain
+            }
+        },
+        'gamDsst' : {
+            'f' : {
+                'gam_dsst_e'     : lambda: self.evt.gam_dsst.e,
+                'gam_dsst_costh' : lambda: self.evt.gam_dsst.costh,
+                'dmdsst'         : lambda: self.evt.dmdsst
+            }
+        },
+        'gamDsstMC' : {
+            # 'i' : {
+            #     'gamDsst_id'   : lambda: self.mcevt.gam_dsst_gen.id,
+            #     'gamDsst_flag' : lambda: self.mcevt.gam_dsst_gen.flag
+            # },
+            'iv' : {
+                'gamDsst_ch' : lambda: self.mcevt.gam_dsst_gen.chain
+            }
+        }
+        }
+
+        mdict = {
+            0  : ['common', 'gamDsj'],  # Ds gamma
+            1  : ['common', 'pi0Dsj'],  # Ds pi0
+            2  : ['common', 'pi+pi-'],  # Ds pi+ pi-
+            3  : ['common', 'pi0pi0'],  # Ds pi0 pi0
+            10 : ['common', 'gamDsj', 'gamDsst'],  # Ds gamma gamma
+            11 : ['common', 'pi0Dsj', 'gamDsst'],  # Ds pi0 gamma
+            12 : ['common', 'pi+pi-', 'gamDsst'],  # Ds pi+ pi- gamma
+            13 : ['common', 'pi0pi0', 'gamDsst'],  # Ds pi0 pi0 gamma
+        }
+
+        self.modeDict = {mode : mdict[mode] for mode in self.modes}
+
+    def getBranches(self, mode, mc):
+        """ Get branches for a mode """
+        ddict = {}
+        for key in ['i', 'f', 'iv']:
+            array = []
+            for d in self.modeDict[mode]:
+                if key in self.branches[d]:
+                    array = array + [var for var in self.branches[d][key].keys()]
+                if mc and (key in self.branches[d + 'MC']):
+                    array = array + [var for var in self.branches[d + 'MC'][key].keys()]
+            ddict[key] = array
+        return ddict
+
+    def getFiller(self, mode, mc):
+        """ Get branches for a mode """
+        ddict = {}
+        for key in ['i', 'f', 'iv']:
+            array = []
+            for d in self.modeDict[mode]:
+                if key in self.branches[d]:
+                    array = array + [(var, fcn) for var, fcn in self.branches[d][key].iteritems()]
+                if mc and (key in self.branches[d + 'MC']):
+                    array = array + [(var, fcn) for var, fcn in self.branches[d + 'MC'][key].iteritems()]
+            ddict[key] = array
+        return ddict
+
+    def ofile(self):
+        """ Output file """
+        return '/'.join([tuplePath, 'signt', '_'.join([self.name, 'sigmc', self.ty, 'st' + self.st, 'split']) + '.root'])
 
     def split(self):
         """ """
-        ofile = TFile('/'.join([tuplePath, 'signt', '_'.join([self.name, 'sigmc', ty, 'st' + str(st), 'split']) + '.root']), 'RECREATE')
-        self.makeTree(0, 'dsj_0')
-        self.t.Draw('>>evlist', DsStr())
+        ofile = TFile(self.ofile(), 'RECREATE')
+        for mode in self.modeDict.keys():
+            self.makeTree(mode, 'dsj_{}'.format(mode))
+        # self.t.Draw('>>evlist', DsStr())
         # evl = TEventList(gDirectory.Get('evlist'))
-        self.t.SetEventList(gDirectory.Get('evlist'))
-        for _ in self.t:
-            if self.evt.mode != 0:
-                continue
-            self.fillCommon()
-            self.fillVec()
-            self.t.Fill()
+        # self.t.SetEventList(gDirectory.Get('evlist'))
+        for idx, _ in enumerate(self.chain):
+            if idx % 10000 == 0:
+                print('{} events'.format(idx))
+            self.fillEvent(self.evt.mode)
+            self.t[self.evt.mode].Fill()
         ofile.Write()
         ofile.Close()
 
     def makeTree(self, mode, name):
         """ Flat TTree for a given mode """
-        br = Branches(mode)
-        self.t = TTree(name, name)
-        self.vars = {}
+        br = self.getBranches(mode, True)
+        self.t[mode] = TTree(name, name)
         for var in br['i']:
-            self.vars[var] = int(0)
-            self.t.Branch(var, self.vars[var], var + '/I')
+            if var not in self.vars:
+                self.vars[var] = np.empty(1, dtype=np.int32)
+            self.t[mode].Branch(var, self.vars[var], var + '/I')
         for var in br['f']:
-            self.vars[var] = float(0)
-            self.t.Branch(var, self.vars[var], var + '/D')
+            if var not in self.vars:
+                self.vars[var] = np.empty(1, dtype=np.float32)
+            self.t[mode].Branch(var, self.vars[var], var + '/F')
         for var in br['iv']:
-            self.vars[var] = np.empty(9, dtype=np.int32)
-            self.t.Branch(var, self.vars[var], var + '[9]/D')
+            if var not in self.vars:
+                self.vars[var] = np.empty(9, dtype=np.int32)
+            self.t[mode].Branch(var, self.vars[var], var + '[9]/I')
 
-    def fillCommon(self):
+    def fillEvent(self, mode):
         """ Fill common variables """
-        # 'i'  : ['exp', 'run', 'evtn', 'mode', 'dsmode', 'flv', 'h_ds_id', 'h_ds_flag', 'tmode'],
-        # 'f'  : ['m', 'mds', 'pcmds', 'cos_hel_dsj'],
-        # 'iv' : ['h_ds_ch']
-        # Integer branches
-        self.vars['exp']    = self.info.exp
-        self.vars['run']    = self.info.run
-        self.vars['evtn']   = self.info.evtn
-        self.vars['mode']   = self.evt.mode
-        self.vars['dsmode'] = self.evt.mode_ds
-        self.vars['flv']    = self.evt.flv
+        filler = self.getFiller(mode, True)
+        for var, fcn in filler['i']:
+            self.vars[var][0] = fcn()
 
-        # MC
-        self.vars['h_ds_id']   = self.mcevt.h_ds_gen.id
-        self.vars['h_ds_flag'] = self.mcevt.h_ds_gen.flag
-        self.vars['tmode'] = self.findDecay(ids['Dsj0+'], 'Dsj') - self.findDecay(ids['Dsj0-'], 'Dsj')\
-                           + 100*self.findDecay(ids['Dsj1+'], 'Dsj') - 100*self.findDecay(ids['Dsj1-'], 'Dsj')
-        self.vars['tdsmode'] = self.findDecay(ids['Ds+'], 'Ds') - self.findDecay(ids['Ds-'], 'Ds')
+        for var, fcn in filler['f']:
+            self.vars[var][0] = fcn()
 
-        # Float branches
-        self.vars['m']     = self.evt.m - self.evt.mds + ModeSplitter.mds
-        self.vars['mds']   = self.evt.mds
-        self.vars['pcmds'] = self.evt.pcmsds
-        self.vars['cos_hel_dsj'] = self.evt.cos_hel_dsj
-        
-        # MC
-        self.vars['h_ds_ch'][:] = self.mcevt.h_ds_gen.ch
-
-    def fillVec(self):
-        """ Fill phi or K*0 variables """
-        self.vars['mvec'] = self.evt.mvec
-        self.vars['cos_hel_vec'] = self.evt.cos_hel_vec
-
-    def findDecay(self, idhep, key):
-        """ Identify mode in the EvtGen table """
-        nphep = np.array([x for x in self.idhep])
-        idxList = np.nonzero(nphep == idhep)
-        if not idxList:
-            return -1
-        decays = []
-        for idx in idxList:
-            fsp = set([self.idhep[i] for i in np.arange(self.daF[idx], self.daL[idx])])
-            for key0, fsp0 in modesDict[key]:
-                if fsp in fsp0:
-                    decays.append(key0)
-        if len(decays) > 1:
-            print('several modes found: {}'.format(decays))
-        return sum(decays)
+        for var, fcn in filler['iv']:
+            self.vars[var][:] = fcn()
 
     def getChain(self):
         """ """
+        self.dschain = TChain('ds', 'ds')
         self.chain = TChain('dsj', 'dsj')
         print(sigMCFile(self.ty, self.st))
         for fname in sigMCFile(self.ty, self.st):
             self.chain.Add(fname)
-        self.intree = self.chain
-        self.intree.GetEvent(0)
-        print('evt' in dir(self.intree))
-        print('{} Dsj candidates'.format(self.intree.GetEntries()))
-        self.evt   = self.intree.evt
-        self.mcevt = self.intree.mcevt
+            self.dschain.Add(fname)
+        self.chain.GetEvent(0)
+        print('{} Dsj candidates'.format(self.chain.GetEntries()))
+        self.evt   = self.chain.evt
+        self.mcevt = self.chain.mcevt
         self.info  = self.evt.info
-        print(dir(self.mcevt))
-        self.idhep = self.mcevt.genhep.idhep
-        self.daF   = self.mcevt.genhep.daF
-        self.daL   = self.mcevt.genhep.daL
-
-    # def splitSigMC(self):
-    #     """ Split signal MC """
-    #     ofile = TFile('/'.join([tuplePath, 'signt', '_'.join([self.name, 'sigmc', ty, 'st' + str(st), 'split']) + '.root']), 'RECREATE')
-    #     otree = {key : dsj.CloneTree(0) for key in self.modes}
-    #     for key, tree in otree.iteritems():
-    #         tree.SetName('dsj_{}'.format(key))
-    #     evt = dsj.evt
-    #     for idx, _ in enumerate(dsj):
-    #         if idx % 100000 == 0:
-    #             print('{} events'.format(idx))
-    #         if evt.mode in modes:
-    #             otree[evt.mode].Fill()
-    #     for key, tree in otree.iteritems():
-    #         print('mode {:02d} : {:07d} events'.format(key, tree.GetEntries()))
-    #     ofile.Write()
-    #     ofile.Close()
 
 def main():
     """ Unit test """
@@ -219,9 +262,8 @@ def main():
     gSystem.Load('libdsjdata.so')
     ms = ModeSplitter('dsjinc', 'dsst0', 1, [0, 1, 2, 3, 10, 11, 12, 13])
     ms.split()
-    # ms.splitSigMC()
+    # ms = ModeSplitter('dsjinc', 'dsst1', 1, [0, 1, 2, 3, 10, 11, 12, 13])
+    # ms.split()
 
 if __name__ == '__main__':
     main()
-
-# makeSigDirTree()
